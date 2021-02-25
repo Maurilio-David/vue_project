8db812f62dd41dcf7bf41d7619388a08ce0f9eb0
Maurílio Conceição <maurilio.conceicao@branef.com.br>
2021-02-25 14:49:51 -0300

    iniciando o projeto

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,85 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <Alunos/> 
+  </div> 
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Alunos from './components/Alunos.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Alunos
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  background-color: #eee;
+  display: grid;
+  justify-items: center;
 }
+
+body,html {
+  margin: 0;
+  height: 100%;
+}
+
+#app {
+  
+}
+.btn-Danger{
+  background-color: #fa4430;
+}
+.btn{
+  padding: 10px 20px;
+  cursor: pointer;
+  border: 1px solid #000;
+  color: white;
+  font-weight: bold;
+  border-radius: 5px;
+  border-bottom: 3px solid #000;
+  border-right: 3px solid #000;
+  border-left: 3px solid #000;
+}
+.btn:hover{
+  text-shadow: 1px 1px 1px black;
+  margin-top: 3px;
+  border-bottom: 1px solid black;
+  border-right: 1px solid #000;
+  border-left: 1px solid #000;
+}
+
+table{
+  margin: 0px;
+  padding: 0px;
+  list-style-type: none;
+  width: 100%;
+  border: none;
+  font-weight: bold;
+}
+
+table th {
+  background-color: rgb(22, 170, 219) !important;
+  font-size: 1.2em;
+  padding: 10px 0px;
+  text-align: center;
+}
+
+tr td {
+  padding: 20px;
+  font-size: white;
+  background-color: #aec5c9;
+  margin-bottom: 2px;
+  color: #3e5252;
+  text-align: center;
+}
+
+.colPequeno{
+  width: 5%;
+}
+
 </style>
---
+++ src/components/Alunos.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <h1>{{titulo}}</h1>
+    <input type="text" placeholder="Nome do Aluno" v-model="nome" v-on:keyup.enter="addAluno()">
+    <button class="btn btn-Input" @click="addAluno(aluno)">Adicionar</button>
+    <br>
+    <table>
+        <th>Mat.</th>
+        <th>Nome</th>
+        <th>Opções</th>
+      <tbody>
+        <tr v-for="(aluno, index) in alunos" :key="index">
+          <td>{{index+1}}</td>
+          <td>{{aluno.nome}}</td>
+          <td>
+            <button class="btn btn-Danger" @click="remover(aluno)">Remover</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+    
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return{
+      titulo: 'Aluno',
+    nome: "",
+      alunos: [
+      {id: 1, nome: 'Juliana'}, 
+      {id: 2, nome: 'Maurílio'}, 
+      {id: 3, nome: 'Isaias'}
+      ]
+    }
+  },
+  props: {
+  },
+  methods: {
+    addAluno() {
+      let _aluno = {
+        nome: this.nome
+      }
+      this.alunos.push(_aluno);
+      this.nome = '';
+    },
+    remover(aluno){
+      let indice = this.alunos.indexOf(aluno);
+      console.log(indice);
+      this.alunos.splice(indice, 1);
+    }
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+input{
+  border: 0;
+  padding: 20px;
+  font-size: 1.3em;
+  color: #587f7f;
+  display: inline;
+}
+
+.btn-Input{
+  width: 150px;
+  border: 0px;
+  padding: 20px;
+  font-size: 1.3rem;
+  display: inline;
+  background-color: rgb(116, 115, 115);
+}
+
+.btn-Input:hover{
+  padding: 20px;
+  margin: 0px;
+  border: 0px;
+}
+</style>
